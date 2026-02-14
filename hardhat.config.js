e6d47bba7ef58cf0acd0bad3fd9c4b66ed76a075
KonstantinVojt <114500793+KonstantinVojt@users.noreply.github.com>
2026-02-14 01:35:08 +0100

    Deploying SyntheticTokenFactory in Sepolia and Verifying the Contract
    - Configure Hardhat for Sepolia network
    - Add environment variables support (.env)
    - Deploy SyntheticTokenFactory contract to Sepolia
    - Verify contract source code on Etherscan
    - Update .gitignore to exclude sensitive files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,25 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("solidity-coverage");
-
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const {
+  SEPOLIA_RPC_URL,
+  PRIVATE_KEY,
+  ETHERSCAN_API_KEY,
+} = process.env;
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL || "",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY || "",
+  },
 };
